7200d3d073e7530520121a5b5a9f7441ca04815e
Patricia P <andrograf31@gmail.com>
2023-05-04 10:39:34 +0200

    feat: make window moveable

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace ChatAppWithWPF
 {
@@ -23,6 +13,14 @@ namespace ChatAppWithWPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove(); //ablak mozgatasa bal egergomb lenyomas eseten
+            }
         }
     }
 }
