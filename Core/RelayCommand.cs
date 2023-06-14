7fd231d7178c31eb93476fbe138f7f691a9d76c5
Patricia P <andrograf31@gmail.com>
2023-06-14 22:07:21 +0200

    feat: implement relay command class

--- Core/RelayCommand.cs
+++ Core/RelayCommand.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ChatAppWithWPF.Core
 {
-    class RelayCommand
+    class RelayCommand : ICommand
     {
+        private Func<object, bool> _canExecuteChanged;
+        private Action<object> _executeChanged;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecuteChanged == null || _canExecuteChanged(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _executeChanged(parameter);
+        }
     }
 }
